621dc88f25c0448ac26744b6a641725eb6a65062
AjayLSojitra <ajaylsojitra@gmail.com>
2025-04-16 09:44:43 +0530

    Upgraded all the dependencies like expo and react native and fix the build creation issue

--- modules/shared/components/lottie-wrapper.tsx
+++ modules/shared/components/lottie-wrapper.tsx
@@ -1,41 +1,37 @@
 import React from "react";
 import { LottieProps } from "react-lottie";
-import { Platform } from "react-native";
 import LottieAppPlayer from "./lottie-app-player";
-import LottieWebPlayer from "./lottie-web-player";
 
-
-function LottieWrapper(props: Readonly<{
-  webProps: LottieProps;
-  source: string;
-  height: number;
-  width: number;
-  loop: boolean;
-  onAnimationLoadeda?: () => void;
-  onAnimationFinisha?: () => void;
-}>) {
-  const { webProps, source, height, width, loop, onAnimationLoadeda, onAnimationFinisha } = props;
+function LottieWrapper(
+  props: Readonly<{
+    webProps: LottieProps;
+    source: string;
+    height: number;
+    width: number;
+    loop: boolean;
+    onAnimationLoadeda?: () => void;
+    onAnimationFinisha?: () => void;
+  }>
+) {
+  const {
+    source,
+    height,
+    width,
+    loop,
+    onAnimationLoadeda,
+    onAnimationFinisha,
+  } = props;
 
   return (
-    <>
-      {Platform.OS === "web" ? (
-        <LottieWebPlayer
-          webProps={webProps}
-          onAnimationLoadeda={onAnimationLoadeda}
-          onAnimationFinisha={onAnimationFinisha}
-        />
-      ) : (
-        <LottieAppPlayer
-          onAnimationLoadeda={onAnimationLoadeda}
-          onAnimationFinisha={onAnimationFinisha}
-          loop={loop}
-          source={source}
-          height={height}
-          width={width}
-        />
-      )}
-    </>
-  )
+    <LottieAppPlayer
+      onAnimationLoadeda={onAnimationLoadeda}
+      onAnimationFinisha={onAnimationFinisha}
+      loop={loop}
+      source={source}
+      height={height}
+      width={width}
+    />
+  );
 }
 
 export default LottieWrapper;
